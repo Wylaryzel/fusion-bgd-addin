3b7994e1cff24f61b42df84c7d99fe017186ced9
Wylaryzel <wylaryzel@gmail.com>
2025-09-29 16:37:57 +0200

    Milestone M2

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,30 +1,9 @@
-# Here you define the commands that will be added to your add-in.
+# commands/__init__.py
+from .paletteShow.entry import start as start_palette_show, stop as stop_palette_show
+# weitere Commands kannst du hier später ergänzen
 
-# TODO Import the modules corresponding to the commands you created.
-# If you want to add an additional command, duplicate one of the existing directories and import it here.
-# You need to use aliases (import "entry" as "my_module") assuming you have the default module named "entry".
-from .commandDialog import entry as commandDialog
-from .paletteShow import entry as paletteShow
-from .paletteSend import entry as paletteSend
-
-# TODO add your imported modules to this list.
-# Fusion will automatically call the start() and stop() functions.
-commands = [
-    commandDialog,
-    paletteShow,
-    paletteSend
-]
-
-
-# Assumes you defined a "start" function in each of your modules.
-# The start function will be run when the add-in is started.
 def start():
-    for command in commands:
-        command.start()
+    start_palette_show()
 
-
-# Assumes you defined a "stop" function in each of your modules.
-# The stop function will be run when the add-in is stopped.
 def stop():
-    for command in commands:
-        command.stop()
+    stop_palette_show()
--- commands/paletteShow/entry.py
+++ commands/paletteShow/entry.py
@@ -1,193 +1,141 @@
-import json
-import adsk.core
-import os
-from ...lib import fusionAddInUtils as futil
-from ... import config
-from datetime import datetime
+import adsk.core, traceback, os, json
 
-app = adsk.core.Application.get()
-ui = app.userInterface
+_app = adsk.core.Application.get()
+_ui  = _app.userInterface
 
-# TODO ********************* Change these names *********************
-CMD_ID = f'{config.COMPANY_NAME}_{config.ADDIN_NAME}_PalleteShow'
-CMD_NAME = 'Show My Palette'
-CMD_Description = 'A Fusion Add-in Palette'
-PALETTE_NAME = 'My Palette Sample'
-IS_PROMOTED = False
+_CMD_ID = 'BGD_ShowPalette'
+_CMD_NAME = 'Boardgame Insert'
+_CMD_DESC = 'Öffnet die Boardgame-Insert Palette'
 
-# Using "global" variables by referencing values from /config.py
-PALETTE_ID = config.sample_palette_id
+_PALETTE_ID = 'BGD_Palette'
+_cmd_def = None
 
-# Specify the full path to the local html. You can also use a web URL
-# such as 'https://www.autodesk.com/'
-PALETTE_URL = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'html', 'index.html')
+# Wir halten Delegates am Leben
+_handlers = []
 
-# The path function builds a valid OS path. This fixes it to be a valid local URL.
-PALETTE_URL = PALETTE_URL.replace('\\', '/')
+def _on_html_event_received(args: adsk.core.HTMLEventArgs):
+    """
+    Empfängt Nachrichten aus dem HTML (window.adsk.fusionJavaScriptHandler.sendData)
+    Erwartet JSON-Strings mit mind. "type"
+    """
+    try:
+        data = json.loads(args.data or '{}')
+        msg_type = data.get('type')
 
-# Set a default docking behavior for the palette
-PALETTE_DOCKING = adsk.core.PaletteDockingStates.PaletteDockStateRight
+        if msg_type == 'ping':
+            # Antworte ins HTML
+            payload = {'type': 'pong', 'ts': adsk.core.ValueInput.createByString('now')}
+            _send_to_html(payload)
 
-# TODO *** Define the location where the command button will be created. ***
-# This is done by specifying the workspace, the tab, and the panel, and the 
-# command it will be inserted beside. Not providing the command to position it
-# will insert it at the end.
-WORKSPACE_ID = 'FusionSolidEnvironment'
-PANEL_ID = 'SolidScriptsAddinsPanel'
-COMMAND_BESIDE_ID = 'ScriptsManagerCommand'
+        elif msg_type == 'save_settings':
+            # Beispiel: einfache Settings im Design als Attribut speichern
+            attrs = data.get('payload', {})
+            _set_design_attributes('bgd_settings', attrs)
+            _send_to_html({'type': 'saved', 'payload': attrs})
 
-# Resource location for command icons, here we assume a sub folder in this directory named "resources".
-ICON_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', '')
+        elif msg_type == 'load_settings':
+            attrs = _get_design_attributes('bgd_settings')
+            _send_to_html({'type': 'settings', 'payload': attrs})
 
-# Local list of event handlers used to maintain a reference so
-# they are not released and garbage collected.
-local_handlers = []
+        else:
+            _ui.messageBox(f'Unbekannter HTML-Event: {msg_type}')
 
+    except Exception:
+        _ui.messageBox('Fehler in _on_html_event_received:\n{}'.format(traceback.format_exc()))
 
-# Executed when add-in is run.
+def _send_to_html(payload: dict):
+    try:
+        pal = _ui.palettes.itemById(_PALETTE_ID)
+        if pal:
+            pal.sendInfoToHTML('bgd-message', json.dumps(payload))
+    except:
+        _ui.messageBox('Fehler beim Senden an HTML:\n{}'.format(traceback.format_exc()))
+
+def _on_command_created(args: adsk.core.CommandCreatedEventArgs):
+    try:
+        pal = _ui.palettes.itemById(_PALETTE_ID)
+        if not pal:
+            html_path = os.path.join(os.path.dirname(__file__), 'resources', 'palette.html')
+            pal = _ui.palettes.add(
+                _PALETTE_ID, 'Boardgame Insert', html_path,
+                True, True, True, 420, 560
+            )
+            # HTML→Python
+            pal.incomingFromHTML.add(_on_html_event_received)
+            _handlers.append(_on_html_event_received)
+
+        pal.isVisible = True
+
+        # Beim Öffnen einmal Settings rüberschicken
+        attrs = _get_design_attributes('bgd_settings')
+        _send_to_html({'type': 'settings', 'payload': attrs})
+
+    except:
+        _ui.messageBox('Fehler beim Erstellen/Öffnen der Palette:\n{}'.format(traceback.format_exc()))
+
 def start():
-    # Create a command Definition.
-    cmd_def = ui.commandDefinitions.addButtonDefinition(CMD_ID, CMD_NAME, CMD_Description, ICON_FOLDER)
+    global _cmd_def
+    _cmd_def = _ui.commandDefinitions.itemById(_CMD_ID)
+    if not _cmd_def:
+        _cmd_def = _ui.commandDefinitions.addButtonDefinition(_CMD_ID, _CMD_NAME, _CMD_DESC)
 
-    # Add command created handler. The function passed here will be executed when the command is executed.
-    futil.add_handler(cmd_def.commandCreated, command_created)
+    _cmd_def.commandCreated.add(_on_command_created)
+    _handlers.append(_on_command_created)
 
-    # ******** Add a button into the UI so the user can run the command. ********
-    # Get the target workspace the button will be created in.
-    workspace = ui.workspaces.itemById(WORKSPACE_ID)
+    tab = _ui.allToolbarTabs.itemById('SolidScriptsAddinsTab')
+    panel = tab.toolbarPanels.itemById('ScriptsAddinsPanel')
+    if panel.controls.itemById(_CMD_ID) is None:
+        panel.controls.addCommand(_cmd_def)
 
-    # Get the panel the button will be created in.
-    panel = workspace.toolbarPanels.itemById(PANEL_ID)
-
-    # Create the button command control in the UI after the specified existing command.
-    control = panel.controls.addCommand(cmd_def, COMMAND_BESIDE_ID, False)
-
-    # Specify if the command is promoted to the main toolbar. 
-    control.isPromoted = IS_PROMOTED
-
-
-# Executed when add-in is stopped.
 def stop():
-    # Get the various UI elements for this command
-    workspace = ui.workspaces.itemById(WORKSPACE_ID)
-    panel = workspace.toolbarPanels.itemById(PANEL_ID)
-    command_control = panel.controls.itemById(CMD_ID)
-    command_definition = ui.commandDefinitions.itemById(CMD_ID)
-    palette = ui.palettes.itemById(PALETTE_ID)
+    # Palette schließen/entfernen
+    try:
+        pal = _ui.palettes.itemById(_PALETTE_ID)
+        if pal:
+            pal.deleteMe()
+    except:
+        pass
 
-    # Delete the button command control
-    if command_control:
-        command_control.deleteMe()
+    # Button entfernen
+    try:
+        tab = _ui.allToolbarTabs.itemById('SolidScriptsAddinsTab')
+        panel = tab.toolbarPanels.itemById('ScriptsAddinsPanel')
+        ctl = panel.controls.itemById(_CMD_ID)
+        if ctl:
+            ctl.deleteMe()
+    except:
+        pass
 
-    # Delete the command definition
-    if command_definition:
-        command_definition.deleteMe()
+    if _cmd_def:
+        _cmd_def.deleteMe()
 
-    # Delete the Palette
-    if palette:
-        palette.deleteMe()
+# --- Kleine Helper für Attribute am Design (persistenter Speicher) ---
 
+def _get_attr_set():
+    try:
+        des = adsk.fusion.Design.cast(_app.activeProduct)
+        if not des:
+            return None, None
+        root = des.rootComponent
+        return des, root.attributes
+    except:
+        return None, None
 
-# Event handler that is called when the user clicks the command button in the UI.
-# To have a dialog, you create the desired command inputs here. If you don't need
-# a dialog, don't create any inputs and the execute event will be immediately fired.
-# You also need to connect to any command related events here.
-def command_created(args: adsk.core.CommandCreatedEventArgs):
-    # General logging for debug.
-    futil.log(f'{CMD_NAME}: Command created event.')
+def _set_design_attributes(group: str, d: dict):
+    des, attrs = _get_attr_set()
+    if not attrs: return
+    for k, v in (d or {}).items():
+        attrs.add(group, str(k), json.dumps(v))
 
-    # Create the event handlers you will need for this instance of the command
-    futil.add_handler(args.command.execute, command_execute, local_handlers=local_handlers)
-    futil.add_handler(args.command.destroy, command_destroy, local_handlers=local_handlers)
-
-
-# Because no command inputs are being added in the command created event, the execute
-# event is immediately fired.
-def command_execute(args: adsk.core.CommandEventArgs):
-    # General logging for debug.
-    futil.log(f'{CMD_NAME}: Command execute event.')
-
-    palettes = ui.palettes
-    palette = palettes.itemById(PALETTE_ID)
-    if palette is None:
-        palette = palettes.add(
-            id=PALETTE_ID,
-            name=PALETTE_NAME,
-            htmlFileURL=PALETTE_URL,
-            isVisible=True,
-            showCloseButton=True,
-            isResizable=True,
-            width=650,
-            height=600,
-            useNewWebBrowser=True
-        )
-        futil.add_handler(palette.closed, palette_closed)
-        futil.add_handler(palette.navigatingURL, palette_navigating)
-        futil.add_handler(palette.incomingFromHTML, palette_incoming)
-        futil.log(f'{CMD_NAME}: Created a new palette: ID = {palette.id}, Name = {palette.name}')
-
-    if palette.dockingState == adsk.core.PaletteDockingStates.PaletteDockStateFloating:
-        palette.dockingState = PALETTE_DOCKING
-
-    palette.isVisible = True
-
-
-# Use this to handle a user closing your palette.
-def palette_closed(args: adsk.core.UserInterfaceGeneralEventArgs):
-    # General logging for debug.
-    futil.log(f'{CMD_NAME}: Palette was closed.')
-
-
-# Use this to handle a user navigating to a new page in your palette.
-def palette_navigating(args: adsk.core.NavigationEventArgs):
-    # General logging for debug.
-    futil.log(f'{CMD_NAME}: Palette navigating event.')
-
-    # Get the URL the user is navigating to:
-    url = args.navigationURL
-
-    log_msg = f"User is attempting to navigate to {url}\n"
-    futil.log(log_msg, adsk.core.LogLevels.InfoLogLevel)
-
-    # Check if url is an external site and open in user's default browser.
-    if url.startswith("http"):
-        args.launchExternally = True
-
-
-# Use this to handle events sent from javascript in your palette.
-def palette_incoming(html_args: adsk.core.HTMLEventArgs):
-    # General logging for debug.
-    futil.log(f'{CMD_NAME}: Palette incoming event.')
-
-    message_data: dict = json.loads(html_args.data)
-    message_action = html_args.action
-
-    log_msg = f"Event received from {html_args.firingEvent.sender.name}\n"
-    log_msg += f"Action: {message_action}\n"
-    log_msg += f"Data: {message_data}"
-    futil.log(log_msg, adsk.core.LogLevels.InfoLogLevel)
-
-    # TODO ******** Your palette reaction code here ********
-
-    # Read message sent from palette javascript and react appropriately.
-    if message_action == 'messageFromPalette':
-        arg1 = message_data.get('arg1', 'arg1 not sent')
-        arg2 = message_data.get('arg2', 'arg2 not sent')
-
-        msg = 'An event has been fired from the html to Fusion with the following data:<br/>'
-        msg += f'<b>Action</b>: {message_action}<br/><b>arg1</b>: {arg1}<br/><b>arg2</b>: {arg2}'               
-        ui.messageBox(msg)
-
-    # Return value.
-    now = datetime.now()
-    currentTime = now.strftime('%H:%M:%S')
-    html_args.returnData = f'OK - {currentTime}'
-
-
-# This event handler is called when the command terminates.
-def command_destroy(args: adsk.core.CommandEventArgs):
-    # General logging for debug.
-    futil.log(f'{CMD_NAME}: Command destroy event.')
-
-    global local_handlers
-    local_handlers = []
+def _get_design_attributes(group: str) -> dict:
+    des, attrs = _get_attr_set()
+    if not attrs: return {}
+    out = {}
+    for i in range(attrs.count):
+        a = attrs.item(i)
+        if a.groupName == group:
+            try:
+                out[a.attributeName] = json.loads(a.value)
+            except:
+                out[a.attributeName] = a.value
+    return out
